72f03eb7afdf61257ff43c0edb04b0f3c8c994ec
root <root@DESKTOP-3FI0Q6V.localdomain>
2022-02-23 22:46:05 -0500

    bug fixes

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from . import views
-from .views import PostList, PostDetail, PostEdit, PostDelete, CommentDelete, Profile, ProfileEdit
+from main_app import views
+from main_app.views import PostList, PostDetail, PostEdit, PostDelete, CommentDelete, Profile, ProfileEdit
 urlpatterns = [
     path('', views.index, name='index'),
 
-    path('home/', views.PostList.as_view(), name='home'),
-    path('home/post/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
-    path('home/post/<int:pk>/edit', views.PostEdit.as_view(), name='post-edit'),
-    path('home/post/<int:pk>/delete', views.PostDelete.as_view(), name='post-delete'),
-    path('home/post/<int:post_pk>/comment/delete/<int:pk>/', views.CommentDelete.as_view(), name='comment-delete'),
+    path('home/', PostList.as_view(), name='home'),
+    path('home/post/<int:pk>/', PostDetail.as_view(), name='post-detail'),
+    path('home/post/<int:pk>/edit', PostEdit.as_view(), name='post-edit'),
+    path('home/post/<int:pk>/delete', PostDelete.as_view(), name='post-delete'),
+    path('home/post/<int:post_pk>/comment/delete/<int:pk>/', CommentDelete.as_view(), name='comment-delete'),
 
-    path('profile/<int:pk>/', views.Profile.as_view(), name='profile'),
-    path('profile/<int:pk>/edit', views.ProfileEdit.as_view(), name='profile-edit'),
+    path('profile/<int:pk>/', Profile.as_view(), name='profile'),
+    path('profile/<int:pk>/edit', ProfileEdit.as_view(), name='profile-edit'),
 
     path('profile/<int:user_id>/add_photo/', views.add_photo, name='add_photo'),
 
